69bb55a1ec656e239e610a96b2c82153bf8d73e9
pwanjiku <cikunjoroge4@gmail.com>
2026-01-30 12:59:31 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-600">
+        Tailwind is working 
+      </h1>
+    </>
+  );
+}
+
+export default App;
