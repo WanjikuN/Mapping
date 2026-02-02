abfeffe5788cdd28ee9ce79995426cf47a4e4d43
pwanjiku <cikunjoroge4@gmail.com>
2026-02-02 14:41:14 +0300

    Update layers container display

--- src/views/MapContainer/Layers.jsx
+++ src/views/MapContainer/Layers.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import  * as React from 'react'
+import { BsLayersFill } from "react-icons/bs";
 const Layers = ({ geoLayers, activeLayers, toggleLayer }) => {
   return (
     <div className="h-full w-[30%] shadow-md bg-white rounded p-1">
       <div className=" bg-[rgba(82,97,41,0.425)] w-full h-full  p-4">
-        <h3 className="font-semibold mb-4">Layers</h3>
+        <h3 className="font-semibold mb-4 uppercase border-b border-white/40 shadow-lg flex flex-row justify-start items-center gap-2 py-2 pl-1">
+          <BsLayersFill size={20} className="text-[rgba(82,97,41,0.97)] " />{" "}
+          <span>Layers</span>
+        </h3>
 
         {geoLayers.map((layer) => (
           <label
